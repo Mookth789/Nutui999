8ea71330c74be915ccc715da6c8166cf4b7165f3
Mookth789 <135083436+Mookth789@users.noreply.github.com>
2023-08-03 22:56:01 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <nut-navbar @on-click-back="back" @on-click-title="title" title="Header">
+    <template #left>
+      <div>back</div>
+    </template>
+    <template #right>
+      <ShareN width="16px" />
+    </template>
+  </nut-navbar>
+
+</template>
+
+<script setup>
+import { ShareN, Cart2, MoreX } from '@nutui/icons-vue';
+const back = () => {
+  console.log('Click Back');
+};
+const title = () => {
+  console.log('Click Title');
+};
+const rightClick = () => {
+  console.log('Click Right');
+};
+const icon = () => {
+  console.log('Click Icon');
+};
+</script>
